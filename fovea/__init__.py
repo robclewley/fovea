a111c7ae27c1b9f3289be9bacc9fceab4b0d7539
robclewley <rob.clewley@gmail.com>
2015-03-25 00:42:14 -0400

    Added new default imports

--- fovea/__init__.py
+++ fovea/__init__.py
@@ -1,3 +1,5 @@
 # fovea interactive dynamics diagnostics, (c) 2015
-import diagnostics
+import graphics
 import common
+import domain2D
+from calc_context import calc_context, general_context, make_measure, prep
