caecce4ccc20d49c915370984d873314311b5704
robclewley <rob.clewley@gmail.com>
2015-05-03 23:55:26 -0400

    Fixed DOI, added plotting layers etc. into the bisection algorithm

--- examples/root_finding/bisection_test1.py
+++ examples/root_finding/bisection_test1.py
@@ -13,7 +13,7 @@ def f1(x):
 # Boilerplate
 
 # domain of interest
-DOI = ([-1,1],[-2,2])
+DOI = ([0,2.1],[-2.5,2])
 
 plotter.clean()
 plotter.addFig('Master',
@@ -21,18 +21,28 @@ plotter.addFig('Master',
                xlabel='x', ylabel='y',
                domain=DOI)
 
-plotter.addLayer('bisect_data')
+plotter.addLayer('fn_data')
+plotter.addLayer('meta_data', kind='text')
 
-
-plotter.arrangeFig([1,1], {'11': {'name': 'plane',
-                                  'scale': DOI,
-                                  'layers': ['bisect_data'],
-                                  'axes_vars': ['x', 'y']}
+plotter.arrangeFig([1,1], {'11':
+                           {'name': 'plane',
+                            'scale': DOI,
+                            'layers': '*',  # all layers will be selected
+                            'axes_vars': ['x', 'y']}
                            })
-
-# Redundant: last added layer becomes active by default
-#plotter.set_active_layer('bisect_data')
 
 gui.buildPlotter2D((8,8), with_times=False)
 
-root = bisection(f1, 0.1, 1)
+plotter.addHLine(0, style='k:', layer='fn_data')
+plotter.addVLine(0, style='k:', layer='fn_data')
+xs = npy.linspace(DOI[0][0], DOI[0][1], 500)
+ys = f1(xs)
+plotter.addData([xs, ys], layer='fn_data', style='k-')
+plt.show()
+
+root = bisection(f1, 0.1, 2)
+
+# demonstration of database log
+db = dm.log.get_DB()
+dblist = db.all()
+n_max = dblist[-1]['n']
--- examples/root_finding/num_modded.py
+++ examples/root_finding/num_modded.py
@@ -24,9 +24,10 @@ import fovea.graphics as gx
 from fovea.graphics import tracker
 from fovea.graphics import gui
 from fovea.diagnostics import diagnostic_manager
+from PyDSTool.Toolbox import phaseplane as pp
 
 global dm
-dm = diagnostic_manager('rootfinding', 'bisect1.log')
+dm = diagnostic_manager('rootfinding', 'bisect1_log.json')
 plotter = gui.plotter
 # -----------------------------------------
 
@@ -37,11 +38,47 @@ def bisection(f, a, b, TOL=0.001, NMAX=100):
     max number of iterations(optional) NMAX and returns the root of the equation
     using the bisection method.
     """
+    def plot_pt(x, n, name, col, marker='o'):
+        """
+        Internal diagnostic helper function.
+        """
+        pt = pp.Point2D(x, f(x))
+        plotter.addPoint(pt, style=col+marker, name=name+'_%d'%n,
+                         layer='bisect_data_%d'%n, log=dm.log)
+        fs = plotter.figs['Master']
+        ax = fs.arrange['11']['axes_obj']
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+        x_ext = xlim[1]-xlim[0]
+        y_ext = ylim[1]-ylim[0]
+        # add marker text at a distance proportional to size of
+        # current axis extent
+        plotter.addText(pt[0]-0.05*x_ext, pt[1]+0.02*y_ext, name,
+                        use_axis_coords=False, name=name+'_%d'%n,
+                        layer='bisect_text_%d'%n, style=col)
+        return pt
+
     n=1
+
+    plotter.addText(0.1, 0.95, 'n=%d'%n, use_axis_coords=True,
+                    name='n_value', layer='meta_data', style='k')
     while n<=NMAX:
         dm.log = dm.log.bind(n=n)
+        plotter.setText('n_value', 'n=%d'%n, 'meta_data')
+        if n == 1:
+            rebuild = True
+        else:
+            plotter.toggleDisplay(layer='bisect_text_%d'%(n-1))
+            plotter.toggleDisplay(layer='bisect_data_%d'%(n-1))
+            rebuild = False
+        plotter.addLayer('bisect_data_%d'%n)
+        plotter.addLayer('bisect_text_%d'%n, kind='text')
         c = (a+b)/2.0
         dm.log.msg('Bisect loop', a=a, b=b, c=c)
+        a_pt = plot_pt(a, n, 'a', 'r', 'o')
+        b_pt = plot_pt(b, n, 'b', 'g', 'o')
+        c_pt = plot_pt(c, n, 'c', 'k', 'x')
+
         if f(c)==0 or (b-a)/2.0 < TOL:
             dm.log.msg('Success', fval=f(c), err=(b-a)/2.0)
             dm.log = dm.log.unbind('n')
@@ -55,6 +92,7 @@ def bisection(f, a, b, TOL=0.001, NMAX=100):
                 dm.log.msg('Opposite sign')
                 b=c
             dm.log.msg('Step', err=(b-a)/2.0)
+        plotter.show(rebuild=rebuild, wait=True)
     dm.log.msg('Failure', status='fail', fval=f(c), err=(b-a)/2.0)
     dm.log = dm.log.unbind('n')
     return False
