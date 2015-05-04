60a90722fbd5fc047fee5e9f7377f03e11f6a654
robclewley <rob.clewley@gmail.com>
2015-05-04 09:25:01 -0400

    Use numpy instead of math to allow vectorization

--- examples/root_finding/test_funcs.py
+++ examples/root_finding/test_funcs.py
@@ -1,7 +1,14 @@
-import math
+import numpy as npy
 
 def f1(x):
     """
     Test function 1
     """
-    return x*x*x - math.pi*x + math.e/100
+    return x*x*x - npy.pi*x + npy.e/100
+
+def f2(x):
+    """
+    Test function 2
+    """
+    return -1.13 + npy.tanh(x-2) + 4*npy.exp(-x)*npy.sin((1/8.)*x**3) \
+           *x + .1*npy.exp((1/35.)*x)
