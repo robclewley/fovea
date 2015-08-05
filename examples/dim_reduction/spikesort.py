f30c5b277e3a7e8ee782922b75a97ed50cdc3913
Alex Kuefler <kuefleralex@gmail.com>
2015-08-04 20:36:51 -0700

    Added tutorial prompts for spike sorting.

--- examples/dim_reduction/spikesort.py
+++ examples/dim_reduction/spikesort.py
@@ -26,7 +26,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 global default_sw
-default_sw = 30
+default_sw = 64
 
 class spikesorter(graphics.diagnosticGUI):
     def __init__(self, title):
@@ -46,8 +46,18 @@ class spikesorter(graphics.diagnosticGUI):
         self.N = len(vs)
         self.traj = numeric_to_traj([vs], 'test_traj', ['x'], ts, discrete=False)
 
+        #Threshold used in martinez et al.
+        self.mthresh = 4*(np.median(np.abs(vs))/0.6475)
+
         self.selected_pcs = []
         self.fovea_setup()
+
+        print("STEP 1:")
+        print("Create a horizontal line of interest by pressing 'l'.")
+        print("Once created, this line can be forced to extent by pressing 'm'.")
+        print("Enter 'ssort.selected_object.update(name = 'thresh')' to identify the line as a threshold for spike detection")
+        print("Once the line is renamed to 'thresh', the arrow keys can be used to move it up and down.")
+        self.tutorial = 'step2'
 
     def fovea_setup(self):
         #Setup code
@@ -89,7 +99,8 @@ class spikesorter(graphics.diagnosticGUI):
                      },
                     '22':
                     {'name': 'Classified Spikes',
-                     'scale': [(-100, 100), (-100, 100)],
+                     #'scale': [(-100, 100), (-100, 100)],
+                     'scale': [(-300, 300), (-300, 300)],
                      'layers':['scores'],
                      'callbacks':'*',
                      'axes_vars': ['x', 'y']
@@ -190,7 +201,13 @@ class spikesorter(graphics.diagnosticGUI):
 
 
     def user_nav_func(self):
-        if self.selected_object.name is 'threshline':
+        if self.selected_object.name is 'thresh':
+            if self.tutorial == 'step2':
+                print("STEP 2: ")
+                print("When thresh is in place, press 'd' to capture each spike crossing the threshold in a bounding box.")
+                print("Each detected spike will be placed in the top right subplot.")
+                self.tutorial = 'step3'
+
             self.plotter.setText('load_perc', 'Loading...', 'loading_text')
             self.plotter.setLayer('loading_text', display= True)
             self.show()
@@ -258,8 +275,10 @@ class spikesorter(graphics.diagnosticGUI):
             result = find_internal_extrema(self.traj.sample(tlo= tlo, thi= thi))
 
             #Center box around spike peak
-            tlo = tlo + result['global_max'][0] - math.floor(search_width/2)
-            #thi = tlo + result['global_max'][0] + round(search_width/2)
+            #tlo = tlo + result['global_max'][0] - math.floor(search_width/2)
+
+            #Place peak at step 20 (as in Martinez et al.)
+            tlo = tlo + result['global_max'][0] - 20
             thi = tlo + search_width
             box_GUI(self, pp.Point2D(tlo, result['global_max'][1]), pp.Point2D(thi, result['global_min'][1]),
                         name= 'spike_box'+str(c), select= False)
@@ -285,13 +304,13 @@ class spikesorter(graphics.diagnosticGUI):
             c += 1
 
         self.plotter.setLayer('loading_text', display=False)
-        self.set_selected_object(self.context_objects['threshline'])
+        self.set_selected_object(self.context_objects['thresh'])
 
         return X
 
     def project_to_PC(self):
-        print('shape proj vecs:', np.column_stack((self.proj_vec1, self.proj_vec2)).shape)
-        print('shape X', self.X.shape)
+        #print('shape proj vecs:', np.column_stack((self.proj_vec1, self.proj_vec2)).shape)
+        #print('shape X', self.X.shape)
         Y = np.dot(self.X, np.column_stack((self.proj_vec1, self.proj_vec2)))
 
         #If moving to a smaller number of spikes, just forcing out data by reassigning names won't work. Must clear.
@@ -311,7 +330,7 @@ class spikesorter(graphics.diagnosticGUI):
         fig_struct, fig = self.plotter._resolveFig(None)
 
         if k== 'd':
-            print('Detecting spikes...')
+            #print('Detecting spikes...')
             try:
                 self.crosses
             except AttributeError:
@@ -322,7 +341,7 @@ class spikesorter(graphics.diagnosticGUI):
             self.X = self.compute_bbox()
 
             #Plot detected spike
-            print('self.X.shape', self.X.shape)
+            #print('self.X.shape', self.X.shape)
             if len(self.X.shape) == 1:
                 self.addDataPoints([list(range(0, len(self.X))), self.X], layer= 'detected', style= 'b-', name= 'spike0', force= True)
 
@@ -333,6 +352,13 @@ class spikesorter(graphics.diagnosticGUI):
                     c += 1
 
             self.show()
+
+            if self.tutorial == 'step3':
+                print("STEP 3: ")
+                print("You can now press 'p' to perform PCA on the detected spikes.")
+                print("The bottom right subplot will display the first 3 principal components (in red, green, and yellow respectively.)")
+                print("The bottom left subplot will show the detected spikes projected onto the first two PCs")
+                self.tutorial = 'step4'
 
         if k == 'p':
             print('doing PCA...')
@@ -346,7 +372,7 @@ class spikesorter(graphics.diagnosticGUI):
             self.proj_vec1 = self.p.get_projmatrix()[:, 0]
             self.proj_vec2 = self.p.get_projmatrix()[:, 1]
 
-            print('proj mat shape: ', self.p.get_projmatrix().shape)
+            #print('proj mat shape: ', self.p.get_projmatrix().shape)
 
             self.addDataPoints([list(range(0, len(self.proj_vec1))) , self.proj_vec1], style= 'r-', layer= 'pcs', linewidth = 2.5, name= 'first_pc', force= True)
             self.addDataPoints([list(range(0, len(self.proj_vec2))) , self.proj_vec2], style= 'g-', layer= 'pcs', linewidth = 2.5, name= 'second_pc', force= True)
@@ -364,9 +390,20 @@ class spikesorter(graphics.diagnosticGUI):
 
             self.project_to_PC()
 
-ssort = spikesorter("SSort")
+            if self.tutorial == 'step4':
+                print("STEP 4: ")
+                print("Use mouse clicks to explore the data.")
+                print("Clicking on detected spikes in the top-right will highlight the corresponding projection in the bottom right (and vice versa).")
+                print("You can also change the set of PCs onto which the data are projected by clicking the desired project PCs in the bottom left")
+                print("Try exploring simdata1_100000.dat, which is a larger dataset and reveals clearer spiking clusters.")
 
-cutoff = 20
+                print("NOTE ALSO: ")
+                print("Creating a bounding box in the upper-left plot and renaming it to 'ref_box', will change the search width of the detected spike.")
+                print("e.g., if you want detected spikes to be 30 msec long, the box's .dx value must be 30.")
+                print("After creating the box, it will be set to the current selected object. You can select the thresh line again by clicking on it.")
+
+
+ssort = spikesorter("SSort")
 
 rets = find_internal_extrema(ssort.traj.sample())
 
@@ -374,17 +411,18 @@ fig_struct, figs = ssort.plotter._resolveFig(None)
 
 halt = True
 
-#threshline = line_GUI(ssort, pt1, pt2)
-threshline = line_GUI(ssort, pp.Point2D(0,15), pp.Point2D(100000, 15), layer='gx_objects', subplot='11',
-                     name='threshline', select=True)
-ssort.navigate_selected_object('down')
+##threshline = line_GUI(ssort, pt1, pt2)
+#threshline = line_GUI(ssort, pp.Point2D(0,ssort.mthresh), pp.Point2D(100000, ssort.mthresh), layer='gx_objects', subplot='11',
+                     #name='thresh', select=True)
+#ssort.navigate_selected_object('down')
+#ssort.navigate_selected_object('up')
 
-##class keyev(object):
-##    pass
-##
-##ev = keyev
-##ev.key = 'down'
-##ssort.key_on(ev)
+###class keyev(object):
+###    pass
+###
+###ev = keyev
+###ev.key = 'down'
+###ssort.key_on(ev)
 
 
-halt = True
+#halt = True
