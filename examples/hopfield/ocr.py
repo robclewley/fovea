38ac843da1eb20f45e9887867196c446d0bb0e95
mcneela <daniel.mcneela@gmail.com>
2016-05-30 09:50:32 -0700

    First version of Hopfield Network example

---
+++ examples/hopfield/ocr.py
@@ -0,0 +1,23 @@
+from hopfield_network import *
+from visuals import * 
+from alphabet import *
+import numpy as np
+from pylab import *
+
+def to_state(letter):
+    return array([1 if char == 'X' else -1 for char in letter.replace('\n','')])
+
+def show_state(state):
+    state = state.reshape((5, 5))
+    imshow(state, cmap=cm.binary, interpolation='nearest')
+    show()
+
+def main():
+    myNet = VisualHopfield(25)
+    alphabet = [A, B, C, D, E, F, G, H, I, J, K, L, M, 
+                N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
+    training_data = [to_state(letter) for letter in alphabet]
+    learning_data = [to_state(l1)]
+    myNet.run_visualization(training_data, learning_data)
+
+main()
